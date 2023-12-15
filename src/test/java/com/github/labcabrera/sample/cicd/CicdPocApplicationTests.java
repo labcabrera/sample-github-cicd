b5c4f03aaffa44167f2ead9d2fd2523ac6eb06e6
labcabrera <lab.cabrera@gmail.com>
2023-12-15 04:05:53 +0100

    Initial import.

---
+++ src/test/java/com/github/labcabrera/sample/cicd/CicdPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.labcabrera.sample.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
